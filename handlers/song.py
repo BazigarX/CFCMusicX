b17efdf84bb39cafa33ffa7a35fd13815c355336
Bazigar X Bots <89571345+Bazigar1929@users.noreply.github.com>
2021-10-31 17:21:03 +0530

    Update song.py

--- handlers/song.py
+++ handlers/song.py
@@ -22,7 +22,7 @@ def song(client, message):
     for i in message.command[1:]:
         query += ' ' + str(i)
     print(query)
-    m = message.reply('𝐅𝐢𝐧𝐝𝐢𝐧𝐠 💫 𝐓𝐡𝐞 𝐒𝐨𝐧𝐠 🎧 𝐅𝐫𝐨𝐦 𝐞𝐒𝐩𝐨𝐫𝐭 ❤️ 𝐒𝐞𝐫𝐯𝐞𝐫 🌎...')
+    m = message.reply('𝗙𝗶𝗻𝗱𝗶𝗻𝗴 💫𝗧𝗵𝗲 𝗦𝗼𝗻𝗴🎧 𝗕𝘆 𝗡𝗼𝗶𝗡𝗼𝗶 ❤️ 𝗕𝗮𝘇𝗶𝗴𝗮𝗿 🌎...')
     ydl_opts = {"format": "bestaudio[ext=m4a]"}
     try:
         results = YoutubeSearch(query, max_results=1).to_dict()
@@ -41,17 +41,17 @@ def song(client, message):
 
     except Exception as e:
         m.edit(
-            "𝐒𝐨𝐧𝐠 🥀 𝐍𝐨𝐭 😔 𝐅𝐨𝐮𝐧𝐝."
+            "𝗦𝗼𝗻𝗴 🥀 𝗡𝗼𝘁 😔 𝗙𝗼𝘂𝗻𝗱."
         )
         print(str(e))
         return
-    m.edit("𝐃𝐨𝐰𝐧𝐥𝐨𝐚𝐝 ✨ 𝐒𝐨𝐧𝐠 🎸 𝐅𝐫𝐨𝐦 𝐞𝐒𝐩𝐨𝐫𝐭 🥀 𝐒𝐞𝐫𝐯𝐞𝐫 🌎...")
+    m.edit("𝗗𝗼𝘄𝗻𝗹𝗼𝗮𝗱 ✨ 𝗦𝗼𝗻𝗴 🎸 𝗙𝗿𝗼𝗺 𝗡𝗼𝗶𝗻𝗼𝗶 🥀 𝗕𝗮𝘇𝗶𝗴𝗮𝗿 🌎...")
     try:
         with youtube_dl.YoutubeDL(ydl_opts) as ydl:
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = '**🎵 𝐔𝐩𝐥𝐨𝐚𝐝𝐞𝐝 𝐁𝐲 :- ✨ @Sanki_Owner ❤️**'
+        rep = '**🎵 𝗨𝗽𝗹𝗼𝗱𝗲𝗱 𝗕𝘆 :- ✨ @BazigarYT ❤️**'
         secmul, dur, dur_arr = 1, 0, duration.split(':')
         for i in range(len(dur_arr)-1, -1, -1):
             dur += (int(dur_arr[i]) * secmul)
@@ -59,7 +59,7 @@ def song(client, message):
         message.reply_audio(audio_file, caption=rep, thumb=thumb_name, parse_mode='md', title=title, duration=dur)
         m.delete()
     except Exception as e:
-        m.edit('𝐘𝐨𝐮𝐭𝐮𝐛𝐞  𝐄𝐫𝐫𝐨𝐫 ❌ 𝐂𝐨𝐧𝐭𝐚𝐜𝐭 𝐎𝐰𝐧𝐞𝐫 🥀 @Sanki_Owner ❤️')
+        m.edit('𝗬𝗼𝘂𝗧𝘂𝗯𝗲 𝗘𝗿𝗿𝗼𝗿 ❌ 𝗖𝗼𝗻𝘁𝗮𝗰𝘁 𝗢𝘄𝗻𝗲𝗿🥀 @BazigarYT ❤️')
         print(e)
 
     try:
