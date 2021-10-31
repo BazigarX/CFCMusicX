ca94030ab8ef76c81973ea3a5bdf47ae0b7d3e07
Bazigar X Bots <89571345+Bazigar1929@users.noreply.github.com>
2021-10-31 19:51:25 +0530

    Update config.py

--- config.py
+++ config.py
@@ -16,3 +16,5 @@ DURATION_LIMIT = int(getenv("DURATION_LIMIT", "60"))
 COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ !").split())
 
 SUDO_USERS = list(map(int, getenv("SUDO_USERS").split()))
+
+OWNER_USERNAME = getenv("OWNER_USERNAME") 
